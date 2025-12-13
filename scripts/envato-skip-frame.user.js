8ec7de456af6a6b2f063e47facb7e6e881cb47b0
Hungnth <thienhungnth@gmail.com>
2025-12-13 10:33:45 +0700

    Update Envato skip iframe script description for clarity

--- scripts/envato-skip-frame.user.js
+++ scripts/envato-skip-frame.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name             Envato skip iframe
-// @description      Skip preview iframe on themeforest.net and codecanyon.net
+// @description      Skip preview iframe on themeforest and codecanyon
+// @author           HungNth
+// @homepage         https://github.com/HungNth/userscripts
 // @match            *://preview.themeforest.net/item/*
 // @match            *://preview.codecanyon.net/item/*
 // @version          1.0.1
 // @run-at           document-end
 // @license          MIT
-// @icon            https://envato.com/favicon.ico
+// @icon             https://envato.com/favicon.ico
 // ==/UserScript==
 
 window.location.replace(
